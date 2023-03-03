7a070df1374b21a165f1743b5b6678ec24b4d429
tristanzucker <tristanzucker@gmail.com>
2023-03-03 19:38:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -75,4 +75,4 @@ async def get_log(row: int) -> dict:
 
 # %% Run app
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
