6bfefde0d9baa33106292b76395363b3f3bb78b3
tristanzucker <tristanzucker@gmail.com>
2023-03-14 20:40:50 +0000

    Add generate df from Google Sheets

--- google_sheets_functions.py
+++ google_sheets_functions.py
@@ -1,13 +1,13 @@
 from apiclient import discovery # pip install google-api-python-client
 from google.oauth2 import service_account # pip install google-auth-httplib2
+import pandas as pd
 
 ## Additional Methods for Google Sheets: 
 # Example of writing data: https://denisluiz.medium.com/python-with-google-sheets-service-account-step-by-step-8f74c26ed28e
 # https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values
 
+# Create connection to Google Sheets API
 def create_google_sheets_service(service_account_info):
-
-    # Create connection to Google Sheets API
 
     scopes = ["https://www.googleapis.com/auth/spreadsheets"]
     credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=scopes)
@@ -15,11 +15,21 @@ def create_google_sheets_service(service_account_info):
 
     return google_sheets_service
 
-def read_data_from_google_sheet(google_sheets_service, sheet_id, sheet_range):
 
-    # Read values from spreadsheet
+# Read values from spreadsheet
+def read_data_from_google_sheet(google_sheets_service, sheet_id, sheet_range):
 
     request = google_sheets_service.spreadsheets().values().get(spreadsheetId=sheet_id, range=sheet_range)
     response = request.execute()
 
     return response.get("values")
+
+# Read values from spreadsheet
+def get_df_from_google_sheet(google_sheets_service, sheet_id, sheet_range):
+
+    request = google_sheets_service.spreadsheets().values().get(spreadsheetId=sheet_id, range=sheet_range)
+    response = request.execute()
+
+    data = response.get("values")
+
+    return pd.DataFrame(data=data[1:], columns=data[0])
