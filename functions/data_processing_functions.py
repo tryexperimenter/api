e26eeb2425b81630c77589edd57c45aab0211c17
tristanzucker <tristanzucker@gmail.com>
2023-04-06 16:12:11 +0100

    Update for supabase data query

--- functions/data_processing_functions.py
+++ functions/data_processing_functions.py
@@ -2,106 +2,102 @@ import pandas as pd
 import numpy as np
 from time import sleep
 from datetime import datetime
-from google_sheets_functions import get_df_from_google_sheet
+from supabase_db_functions import supabase_get_experimenter_log_data
 from honeybadger import honeybadger
 import traceback
 
-# add some sample text
+## Load variables from .env file or OS environment variables
+from supabase import create_client, Client
+import os
+from dotenv import dotenv_values # pip install python-dotenv
+env_vars = {
+    **dotenv_values(r"C:/Users/trist/experimenter/api/.env"),
+    **os.environ,  # override loaded values with environment variables
+}
 
-def get_experimenter_log_helper(log_id, google_sheets_service, logger):
+## Intialize Supabase client
+supabase_url: str = env_vars.get("SUPABASE_URL")
+supabase_public_api_key: str = env_vars.get("SUPABASE_PUBLIC_API_KEY")
+supabase_client: Client = create_client(supabase_url, supabase_public_api_key)
+
+#Akua: 255243
+#Tristan: f1c14b
+
+response = supabase_client.rpc(
+            fn = "get_experimenter_log_data", 
+            params = {"public_user_id": "f1c14b"}).execute()
+
+df = pd.DataFrame(response.data)         
+
+def get_experimenter_log_helper(public_user_id, supabase_client, logger):
+
+# Deal with each case:
+# 1. public_user_id not found
+# 2. public_user_id found but no experiments to display
+# 3. public_user_id found and experiments to display
+# Probably need to do multiple with statements (first for user_lookups, then for experiments, then for observations)
 
     try:
-        ## Pull data
 
-        # Set Experimenter Database sheet_id
-        sheet_id = "10Lt6tlYRfFSg5KBmF-xCOvdh6shfa1yuvgD2J5z6rbU"
+        ## Pull data from Supabase
+        logger.info("Started pulling data from Supabase")
+        df = supabase_get_experimenter_log_data(public_user_id = public_user_id, supabase_client=supabase_client, logger=logger)
+        logger.info("Finished pulling data from Supabase")
 
-        # Pull data (note: we can't loop through tuples of sheet_ranges and use exec() without throwing an error that the df_users doesn't exist later on... probably because it takes too long to return)
-        logger.info("Started pulling data from Google Sheets")
-        df_users_logs = get_df_from_google_sheet(google_sheets_service=google_sheets_service, sheet_id = sheet_id, sheet_range = 'users_logs!A1:Z1000', logger = logger)    
-        df_users = get_df_from_google_sheet(google_sheets_service=google_sheets_service, sheet_id = sheet_id, sheet_range = 'users!A1:Z1000', logger = logger)
-        df_experiment_groups = get_df_from_google_sheet(google_sheets_service=google_sheets_service, sheet_id = sheet_id, sheet_range = 'experiment_groups!A1:Z1000', logger = logger)
-        df_experiment_sub_groups = get_df_from_google_sheet(google_sheets_service=google_sheets_service, sheet_id = sheet_id, sheet_range = 'experiment_sub_groups!A1:Z1000', logger = logger)
-        df_users_experiment_sub_groups = get_df_from_google_sheet(google_sheets_service=google_sheets_service, sheet_id = sheet_id, sheet_range = 'users_experiment_sub_groups!A1:Z1000', logger = logger)
-        df_experiments = get_df_from_google_sheet(google_sheets_service=google_sheets_service, sheet_id = sheet_id, sheet_range = 'experiments!A1:Z1000', logger = logger)
-        df_observation_prompts = get_df_from_google_sheet(google_sheets_service=google_sheets_service, sheet_id = sheet_id, sheet_range = 'observation_prompts!A1:Z1000', logger = logger)
-        df_observations = get_df_from_google_sheet(google_sheets_service=google_sheets_service, sheet_id = sheet_id, sheet_range = 'observations!A1:Z1000', logger = logger)
-        logger.info("Finished pulling data from Google Sheets")
+        ## CASE 1: Public_user_id was not found / not active
+        # Test: No rows in df
+        # Outcome: Return error 
+        if len(df) == 0:
 
-        # Rename id to table_name_id (e.g., id > user_id)
-        logger.info("Renaming id columns")
-        tuples = [
-            ['users', 'user_id'],
-            ['experiment_groups', 'experiment_group_id'],
-            ['experiment_sub_groups', 'experiment_sub_group_id'],
-            ['experiments', 'experiment_id'],
-            ['observation_prompts', 'observation_prompt_id'],
-            ['observations', 'observation_id'],
-        ]
-        for tuple in tuples:
-            exec(f"df_{tuple[0]}.rename(columns={{'id': '{tuple[1]}'}}, inplace=True)")
-
-        ## Format data
-        logger.info("Formatting data")
-        tuples = [
-            ['df_users_experiment_sub_groups', 'assigned_date'],
-            ['df_observations', 'observation_date'],
-        ]
-        for tuple in tuples:
-            exec(f"{tuple[0]}['{tuple[1]}'] = pd.to_datetime({tuple[0]}['{tuple[1]}'], infer_datetime_format=True)")
-
-        ## Restrict data
-        logger.info("Restricting data")
-
-        # Restrict to relevant log
-        df_users_logs = df_users_logs[df_users_logs['log_id'] == log_id]
-
-        if len(df_users_logs) == 0:
-
-            info_message = f"log_id not found in df_users_logs for log_id: {log_id}"
+            info_message = f"user_lookups table did not contain active public_user_id of '{public_user_id}'"
             logger.info(info_message)
 
             sleep(3) # Sleep to prevent brute force attacks
 
-            return {"error": "True", "end_user_error_message": f"Error collecting Experimenter Log data for log_id: {log_id}"}
+            return {"error": "True", "end_user_error_message": f"Error collecting Experimenter Log data for public_user_id: {public_user_id}"}
 
-        # Restrict to experiment groups that should be visible to user
-        # Note that all data from Google Sheets is read in as string, so we test equality with "1"
+        ## Initialize response dictionary
+        dict_response = {}
+        dict_response["public_user_id"] = public_user_id
+        dict_response["first_name"] = df.first_name.get(0)
+        dict_response['error'] = "False"
+
+        ## CASE 2: User exists but has no assigned experiments
+        # Test:  df has just one row with just user's info (e.g., experiment_group = None)
+        # Outcome: Return early with experiments_to_display = False 
+        if len(df) == 1 and df.experiment.get(0) == None:
+            dict_response['experiments_to_display'] = "False"
+            logger.info(f"No experiments to display for public_user_id: {public_user_id}")
+
+            return dict_response
+
+        ## Format data
+        logger.info("Formatting data")
+
+        # Convert datetime strings to datetime objects
+        date_vars = ["display_datetime"]
+        for date_var in date_vars:
+            exec(f"df['{date_var}'] = pd.to_datetime(df['{date_var}'], infer_datetime_format=True)")
+
+
+        ## Restrict data
+        logger.info("Restricting data")
+
+        # Restrict to experiment_sub_groups that should be visible to user (e.g., action_datetime is before today at local timezone??)
+        
         df_users_experiment_sub_groups.query('visible_to_user == "1"', inplace=True)
-
-        ## Construct response df
-        logger.info("Constructing response df")
-        tuples = [
-            #[left df, right df, join variables]
-            ['df_users_logs', 'df_users', ['user_id']],
-            ['df', 'df_users_experiment_sub_groups', ['user_id']],
-            ['df', 'df_experiment_sub_groups', ['experiment_sub_group_id']],
-            ['df', 'df_experiment_groups', ['experiment_group_id']],
-            ['df', 'df_experiments', ['experiment_sub_group_id']],
-            ['df', 'df_observation_prompts', ['experiment_id']],
-            ['df', 'df_observations', ['user_id','experiment_id','observation_prompt_id']],
-        ]
-        for tup in tuples:
-            df = eval(f"pd.merge({tup[0]}, {tup[1]}, left_on={tup[2]}, right_on={tup[2]}, how='left')")
 
         # Replace missing values (NaN) with "" as NaN is not acceptable in final JSON output
         df.replace({np.nan: ""}, inplace = True)
 
-        # Initialize response dictionary
-        dict_response = {}
-        dict_response["log_id"] = log_id
-        dict_response["first_name"] = df.first_name.get(0)
+        # Update response dictionary
+        dict_response["days_of_experimenting"] = 1 # placeholder if we have not yet assigned experiments
+        dict_response['experiments_to_display'] = "True"
         dict_response["days_of_experimenting"] = max(1, (datetime.today() - df['assigned_date'].min()).days + 1) # Add 1 to include today, take max in case there are no assigned dates
         dict_response['experiments_to_display'] = "True"
-        dict_response['error'] = "False"
+        
+        # Initialize helper array
         array_experiment_groups = []
-
-        # Return if no experiments found for user (e.g., every experiment id is '')
-        experiment_ids = df.experiment_id.unique() 
-        if len(experiment_ids) == 1 and experiment_ids[0] == '':
-            dict_response['experiments_to_display'] = "False"
-            logger.info(f"No experiments to display for log_id: {log_id}")
-            return dict_response
 
         # Collect data for each experiment group
         for experiment_group_id in df.experiment_group_id.unique():
@@ -148,18 +144,18 @@ def get_experimenter_log_helper(log_id, google_sheets_service, logger):
         # Add experiment groups, sub_groups, experiments, and observations to the response dictionary
         dict_response['experiment_groups'] = array_experiment_groups
 
-        logger.info(f"Successfully ran get_experimenter_log_helper() for log_id: {log_id}")
+        logger.info(f"Successfully ran get_experimenter_log_helper() for public_user_id: {public_user_id}")
         return dict_response
     
     # Catch any exceptions as we tried to execute the function
     except Exception as e:
 
         error_class = f"API | get_experimenter_log_helper()"
-        error_message = f"Error with /v1/experimenter-log/?log_id={log_id}; Error: {e}"
+        error_message = f"Error with /v1/experimenter-log/?public_user_id={public_user_id}; Error: {e}"
         logger.error(error_message)
         logger.error(traceback.format_exc()) # provide the full traceback of everything that caused the error
         honeybadger.notify(error_class=error_class, error_message=error_message)
 
-        return {"error": "True", "end_user_error_message": f"Error collecting Experimenter Log data for log_id: {log_id}"}
+        return {"error": "True", "end_user_error_message": f"Error collecting Experimenter Log data for public_user_id: {public_user_id}"}
     
     
